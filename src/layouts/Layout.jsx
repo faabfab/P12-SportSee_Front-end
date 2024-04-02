c5933dc2ec10112f5333fad17c9296573448a278
faabfab <faab.fab@free.fr>
2024-04-02 18:22:42 +0200

    harder

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/Header'
-import LeftNav from '../components/LeftNav'
+import Header from './../components/Header'
+import LeftNav from './../components/LeftNav'
 import { Outlet } from 'react-router-dom'
 import './../scss/layout/layout.scss'
 
