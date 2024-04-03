a347385c15ea2a1c6a60911fd3e81e6852be3858
faabfab <faab.fab@free.fr>
2024-04-03 11:12:39 +0200

    retry

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './../components/Header'
-import LeftNav from './../components/LeftNav'
+import Header from './../components/Header.jsx'
+import LeftNav from './../components/LeftNav.jsx'
 import { Outlet } from 'react-router-dom'
 import './../scss/layout/layout.scss'
 
